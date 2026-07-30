d723d3a4b466cf28b2e847e0cd4a75b98b8edd10
Reza Hassani <288577514+hassanireza@users.noreply.github.com>
2026-07-30 23:35:01 +0330

    update

--- src/hooks/useCatalog.ts
+++
@@ -1,7 +0,0 @@
-import { useServices } from './useServices';
-
-/** Thin accessor for the static ProductCatalog instance. */
-export function useCatalog() {
-  const { catalog } = useServices();
-  return catalog;
-}
